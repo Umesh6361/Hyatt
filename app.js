3039e5b973dff0ca9e350d9fcbb71188a959785d
Umesh <kuwar51@gmail.com>
2021-03-13 02:03:20 +0530

    query added

---
+++ app.js
@@ -0,0 +1,11 @@
+const toggle=document.querySelector("#toggle_btn");
+const divlist=document.querySelector(".features");
+toggle.addEventListener('click', ()=>{
+    if (divlist.style.display==='block') {
+        divlist.style.display='none';
+        toggle.innerHTML="amenities &#x2191;" 
+    } else {
+        divlist.style.display='block'
+        toggle.innerHTML="amenities &#x2193;" 
+    }
+})
