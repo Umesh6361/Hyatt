6641015e8d5376e62c4f2eaabb7fbec2072945bd
Umesh <kuwar51@gmail.com>
2021-03-15 17:01:45 +0530

    event click not working

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 
-    const toggle=document.querySelector(".toggle_btn");
-    const divlist=document.querySelector(".features");
-    toggle.addEventListener('click', ()=>{
-        if (divlist.style.display==='block') {
-            divlist.style.display='none';
-            toggle.innerHTML="amenities &#x2191;" 
-        } else {
-            divlist.style.display='block'
-            toggle.innerHTML="amenities &#x2193;" 
-        }
-    });
-    
-
+const toggle = document.querySelectorAll(".toggle_btn").forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        const divlist = document.querySelectorAll(".features").forEach(divlist=>{
+            if(divlist.style.display === 'block') {
+                divlist.style.display = 'none';
+        toggle.innerHTML = "amenities &#x2191;"
+    } else {
+        divlist.style.display = 'block'
+                toggle.innerHTML = "amenities &#x2193;"
+    }
+        })
+           
+    })
+})
